28fbfb2af769ce6c886365b76a55f1391f059183
Joe Blau <josephblau@gmail.com>
2014-08-02 12:55:02 -0700

    Initial Commit

---
+++ Ra-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ra-iOS
+//
+//  Created by Joe Blau on 8/2/14.
+//  Copyright (c) 2014 ra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ Ra-iOSTests/Ra_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  Ra_iOSTests.swift
+//  Ra-iOSTests
+//
+//  Created by Joe Blau on 8/2/14.
+//  Copyright (c) 2014 ra. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Ra_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
